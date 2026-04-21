205b487cbefac55b9a72e0fbe5eecd4599e74607
Deniss Cvetkovs <cvetkovsdeniss4@gmail.com>
2026-04-21 11:44:51 +0300

    Added Post model

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/test', function (Request $request) {
+    return ["key1" => "value1"];
+});
+
+Route::get('/posts', function(){
+    return Post::all();
+});
