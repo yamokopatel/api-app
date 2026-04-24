91f68a4d4f331def421cc798451d9ddacae20799
Deniss Cvetkovs <cvetkovsdeniss4@gmail.com>
2026-04-24 14:39:08 +0300

    Added new post creation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,4 +16,26 @@ class PostController extends Controller
     {
         return Post::all()->find($id);
     }
+
+    public function store(Request $request)
+    {
+        $values = $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required'
+        ]);
+        $new = Post::create($values);
+        //return Post::all()->find($new->id);
+        //return Route::redirect('api/posts','api/posts/{$new->id}');
+        return $new;
+    }
+
+    public function edit(Request $request, $id)
+    {
+
+    }
+
+    public function destroy($id)
+    {
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,17 +5,17 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 use App\Http\Controllers\PostController;
 
-Route::get('/user', function (Request $request) {
+/*Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware('auth:sanctum');*/
 
-Route::get('/test', function (Request $request) {
+/*Route::get('/test', function (Request $request) {
     return ["key1" => "value1"];
-});
+});*/
 
-Route::get('/posts', function(){
+/*Route::get('/posts', function(){
     return Post::all();
-});
+});*/
 
-Route::get('/', [PostController::class, 'index']);
-Route::get('/{id}', [PostController::class, 'show']);
+Route::apiResource('/posts', PostController::class);
+//Route::get('/{id}', [PostController::class, 'show']);
