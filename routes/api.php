7ecfaca36cb721b97ad59f89da125ee10009573c
Deniss Cvetkovs <cvetkovsdeniss4@gmail.com>
2026-04-24 13:37:51 +0300

    Added object showing functions and routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
+use App\Http\Controllers\PostController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -15,3 +16,6 @@ Route::get('/test', function (Request $request) {
 Route::get('/posts', function(){
     return Post::all();
 });
+
+Route::get('/', [PostController::class, 'index']);
+Route::get('/{id}', [PostController::class, 'show']);
